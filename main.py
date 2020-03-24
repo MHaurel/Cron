e3ad31f32ffc9f1cf251109751b00ae02e1b43e7
MHaurel <54206618+MHaurel@users.noreply.github.com>
2020-03-24 22:44:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from package.job import news
 # Will be scheduled to 20:00
 schedule.every().days.at("20:00").do(news)
 
-while 1:
+while 1:  # Make the program always running
     schedule.run_pending()
     time.sleep(1)
